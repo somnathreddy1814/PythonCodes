b43e101f1d5e2bfaad9c5d27874bf26446f4ceeb
somnathreddy1814 <somnathreddy1814@gmail.com>
2023-09-24 00:24:19 +0530

    Greedy-3

---
+++ job_seq.py
@@ -0,0 +1,28 @@
+class job:
+    def __init__(self,id,deadline,profit):
+        self.id=id
+        self.deadline=deadline
+        self.profit=profit
+class Solution:
+    def jobScheduling(self,jobs):
+        jobs.sort(key=lambda x:x.profit,reverse=True)
+        maxi=jobs[0].deadline
+        for i in range(1,len(jobs)):
+            maxi=max(maxi,jobs[i].profit)
+        
+        slot=[-1]*(maxi+1)
+        cntJobs=0
+        MaxProfit=0
+
+        for i in range(len(jobs)):
+            for j in range(jobs[i].deadline,0,-1):
+                if slot[j]==-1:
+                    slot[j]=i
+                    cntJobs+=1
+                    MaxProfit+=jobs[i].profit
+                    break
+        return cntJobs,MaxProfit
+if __name__ == "__main__":
+    jobs = [job(1, 4, 20), job(2, 1, 10), job(3, 2, 40), job(4, 2, 30)]
+    count, profit = Solution().jobScheduling(jobs)
+    print(count, profit)
