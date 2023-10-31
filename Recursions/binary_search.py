f83dbe816ea6a1c714d7f76a5366859ab2086778
somnathreddy1814 <somnathreddy1814@gmail.com>
2023-10-31 08:05:29 +0530

    bs using recursion

---
+++ Recursions/binary_search.py
@@ -0,0 +1,19 @@
+def bs(arr,n,target,low,high):
+    
+    mid=(low+high)//2
+    if arr[mid]==target:
+        return mid
+    elif arr[mid]>target:
+        return bs(arr,n,low,mid-1,target)
+    elif arr[mid]<target:
+        return bs(arr,n,mid+1,high,target)
+    return -1
+
+
+
+if __name__=="__main__":
+    n=6
+    arr=[1,2,3,4,5,6]
+    target=4
+    print(bs(arr,n,target,0,4))
+
